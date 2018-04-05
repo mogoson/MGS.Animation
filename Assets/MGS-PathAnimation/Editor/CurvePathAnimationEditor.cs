246b1b03dee6999510479b8cd45a0aa42a95665a
Mogoson <mogoson@qq.com>
2018-04-05 17:05:07 +0800

    Modify GenericEditor.

--- Assets/MGS-PathAnimation/Editor/CurvePathAnimationEditor.cs
+++ Assets/MGS-PathAnimation/Editor/CurvePathAnimationEditor.cs
@@ -10,7 +10,7 @@
  *  DeTargetion  :  Initial development version.
  *************************************************************************/
 
-using Mogoson.EditorExtension;
+using Mogoson.UEditor;
 using UnityEditor;
 using UnityEngine;
 

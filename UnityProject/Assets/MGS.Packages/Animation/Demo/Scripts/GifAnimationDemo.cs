e874772a1c7d76ce47dcc8006d4fefdb6e65dc45
Mogoson <mogoson@outlook.com>
2021-10-05 11:11:06 +0800

    Update project files

---
+++ UnityProject/Assets/MGS.Packages/Animation/Demo/Scripts/GifAnimationDemo.cs
@@ -0,0 +1,56 @@
+/*************************************************************************
+ *  Copyright © 2019 Mogoson. All rights reserved.
+ *------------------------------------------------------------------------
+ *  File         :  GifAnimationDemo.cs
+ *  Description  :  Test play gif animation.
+ *------------------------------------------------------------------------
+ *  Author       :  Mogoson
+ *  Version      :  0.1.0
+ *  Date         :  4/20/2019
+ *  Description  :  Initial development version.
+ *************************************************************************/
+
+using MGS.Graph;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace MGS.UAnimation.Demo
+{
+    [RequireComponent(typeof(RawImageAnimation))]
+    public class GifAnimationDemo : MonoBehaviour
+    {
+        public Button playBtn;
+        public Scrollbar progressBar;
+        private RawImageAnimation imgAnim;
+        private string gifFile;
+
+        private void Awake()
+        {
+            imgAnim = GetComponent<RawImageAnimation>();
+            playBtn.onClick.AddListener(OnPlayBtnClick);
+            gifFile = string.Format("{0}/MGS.Packages/Animation/Demo/Textures/Running.gif", Application.dataPath);
+        }
+
+        private void OnPlayBtnClick()
+        {
+            playBtn.gameObject.SetActive(false);
+            progressBar.gameObject.SetActive(true);
+            StartCoroutine(Texture2DUtility.LoadGifAsync(gifFile, OnGifLoading, OnGifLoaded));
+        }
+
+        private void OnGifLoading(float progress, Texture2D tex)
+        {
+            progressBar.size = progress;
+        }
+
+        private void OnGifLoaded(List<Texture2D> texs)
+        {
+            progressBar.gameObject.SetActive(false);
+
+            var frames = texs.ConvertAll(item => { return item as Texture; });
+            imgAnim.SetFrames(frames);
+            imgAnim.Play();
+        }
+    }
+}
